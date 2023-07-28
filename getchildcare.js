33d9d614aec9cea75213aaad6a44630be723b370
MichelleProjectsSoftware <michellsoftwaredev@gmail.com>
2023-07-27 21:07:09 -0600

    add function for filter triggered by the drowpdown

--- getchildcare.js
+++ getchildcare.js
@@ -13,6 +13,7 @@ function pageSetup() {
   document.getElementById("address").addEventListener("keyup", function (){ searchByAddress(this.value);},false);
   document.getElementById("typeProgram").addEventListener("keyup", function (){ searchByType(this.value);},false);
   document.getElementById("capacity").addEventListener("keyup", function () { filterByCapacity(this.value); }, false);
+  document.getElementById("filter").addEventListener('change', function () { filterDrorpDown(this.value); }, false);
   xhr.onreadystatechange = function() {
     if (xhr.readyState == 4 && xhr.status == 200) {
      parsedrecord = JSON.parse(xhr.responseText);
@@ -114,7 +115,6 @@ function searchByType(searchdata)
 function filterByCapacity(searchdata) {
     // Get the capacity filter value from the input field
     var filter = document.getElementById("filter").value;
-    console.log(filter)
   
     // Set up table
     var output = "<tr><th>Program Name</th><th>Program Address</th><th>Type of Program</th><th>Phone Number</th><th>Capacity</th></tr>";
@@ -150,6 +150,60 @@ function filterByCapacity(searchdata) {
         output += record.capacity;
         output += "</td><tr>";
       } else if (filter === "equal to" && capacity == searchdata) {
+        output += "<tr><td>";
+        output += record.program_name;
+        output += "</td><td>"
+        output += record.program_address;
+        output += "</td><td>";
+        output += record.type_of_program;
+        output += "</td><td>";
+        output += record.phone_number;
+        output += "</td><td>";
+        output += record.capacity;
+        output += "</td><tr>";
+      }
+    }
+    document.getElementById("searchresults").innerHTML = output;
+  }
+
+  function filterDrorpDown(filter) {
+    // Get the capacity filter value from the input field
+    var capInput = document.getElementById("capacity").value;
+  
+    // Set up table
+    var output = "<tr><th>Program Name</th><th>Program Address</th><th>Type of Program</th><th>Phone Number</th><th>Capacity</th></tr>";
+    var capacity;
+    for (var i = 0; i < parsedrecord.length; i++) {
+      var record = parsedrecord[i];
+      // Check
+      capacity = parseInt(record.capacity); // Parse the capacity value as an integer
+  
+      // Perform the appropriate filter based on the filterType
+      if (filter === "more than" && capacity > capInput) {
+        output += "<tr><td>";
+        output += record.program_name;
+        output += "</td><td>"
+        output += record.program_address;
+        output += "</td><td>";
+        output += record.type_of_program;
+        output += "</td><td>";
+        output += record.phone_number;
+        output += "</td><td>";
+        output += record.capacity;
+        output += "</td><tr>";
+      } else if (filter === "less than" && capacity < capInput) {
+        output += "<tr><td>";
+        output += record.program_name;
+        output += "</td><td>"
+        output += record.program_address;
+        output += "</td><td>";
+        output += record.type_of_program;
+        output += "</td><td>";
+        output += record.phone_number;
+        output += "</td><td>";
+        output += record.capacity;
+        output += "</td><tr>";
+      } else if (filter === "equal to" && capacity == capInput) {
         output += "<tr><td>";
         output += record.program_name;
         output += "</td><td>"
