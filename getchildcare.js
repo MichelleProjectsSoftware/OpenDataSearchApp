efc87594f84eb97eb62c263d780facd98243d732
MichelleProjectsSoftware <michellsoftwaredev@gmail.com>
2023-07-27 18:39:16 -0600

    add a capacity filter for the childcare program

--- getchildcare.js
+++ getchildcare.js
@@ -12,6 +12,7 @@ function pageSetup() {
   document.getElementById("program").addEventListener("keyup", function (){ searchByProgram(this.value);},false);
   document.getElementById("address").addEventListener("keyup", function (){ searchByAddress(this.value);},false);
   document.getElementById("typeProgram").addEventListener("keyup", function (){ searchByType(this.value);},false);
+  document.getElementById("capacity").addEventListener("keyup", function () { filterByCapacity(this.value); }, false);
   xhr.onreadystatechange = function() {
     if (xhr.readyState == 4 && xhr.status == 200) {
      parsedrecord = JSON.parse(xhr.responseText);
@@ -109,3 +110,58 @@ function searchByType(searchdata)
     document.getElementById("searchresults").innerHTML=output;
 
 }
+
+function filterByCapacity(searchdata) {
+    // Get the capacity filter value from the input field
+    var filter = document.getElementById("filter").value;
+    console.log(filter)
+  
+    // Set up table
+    var output = "<tr><th>Program Name</th><th>Program Address</th><th>Type of Program</th><th>Phone Number</th><th>Capacity</th></tr>";
+    var capacity;
+    for (var i = 0; i < parsedrecord.length; i++) {
+      var record = parsedrecord[i];
+      // Check
+      capacity = parseInt(record.capacity); // Parse the capacity value as an integer
+  
+      // Perform the appropriate filter based on the filterType
+      if (filter === "more than" && capacity > searchdata) {
+        output += "<tr><td>";
+        output += record.program_name;
+        output += "</td><td>"
+        output += record.program_address;
+        output += "</td><td>";
+        output += record.type_of_program;
+        output += "</td><td>";
+        output += record.phone_number;
+        output += "</td><td>";
+        output += record.capacity;
+        output += "</td><tr>";
+      } else if (filter === "less than" && capacity < searchdata) {
+        output += "<tr><td>";
+        output += record.program_name;
+        output += "</td><td>"
+        output += record.program_address;
+        output += "</td><td>";
+        output += record.type_of_program;
+        output += "</td><td>";
+        output += record.phone_number;
+        output += "</td><td>";
+        output += record.capacity;
+        output += "</td><tr>";
+      } else if (filter === "equal to" && capacity == searchdata) {
+        output += "<tr><td>";
+        output += record.program_name;
+        output += "</td><td>"
+        output += record.program_address;
+        output += "</td><td>";
+        output += record.type_of_program;
+        output += "</td><td>";
+        output += record.phone_number;
+        output += "</td><td>";
+        output += record.capacity;
+        output += "</td><tr>";
+      }
+    }
+    document.getElementById("searchresults").innerHTML = output;
+  }
